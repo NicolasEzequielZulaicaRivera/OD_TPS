9fa2373da05bdf6bd72a47dd0e2cc219c5fef87e
ariedro <ariedro@gmail.com>
2021-09-25 18:20:22 -0300

    report inicial para hipotesis

--- adicional.py
+++ adicional.py
@@ -2,6 +2,14 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-ft = pd.read_csv("datasets/features.csv")
+df = pd.read_csv("datasets/features.csv")
 
-ft.sample(10)
+df2 = pd.read_csv("datasets/target.csv")
+
+df = pd.merge(df, df2, on='id', how='outer')
+
+from pandas_profiling import ProfileReport
+
+report = ProfileReport(df, title='Llovieron hamburguesas', explorative=True, lazy=False)
+
+report
--- entrega.py
+++ entrega.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # # TP: Cuidado! Lluvia de Hamburgesas - Primera Entrega
 
 # ### Introducción
