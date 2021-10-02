320772ef7f1702a805eefb31c453d91248372454
nicolas zulaica <nzulaica@fi.uba.ar>
2021-10-02 19:50:11 -0300

    pruebas

--- prueba.py
+++ prueba.py
@@ -44,7 +44,6 @@ filtrosDebiles = [
     df["horas_de_sol"]<7,
     df["humedad_tarde"]>60,
     df["nubosidad_tarde"]>7,
-    df["direccion_viento_tarde"].isin(['Noroeste', 'Oestenoroeste', 'Norte', 'Oeste']),
     df["rafaga_viento_max_direccion"].isin(['Noroeste', 'Oestenoroeste', 'Norte', 'Oeste', 'Nornoreste', 'Oestesuroeste' ]),
     df["barrio"].isin(['Parque Patricios', 'Villa Pueyrredón', 'Saavedra', 'Chacarita', 'Recoleta', 'Vélez Sársfield', 'Barracas', 'Villa del Parque', 'Villa Devoto', 'Caballito', 'Monte Castro', 'La Boca', 'Villa Soldati', 'San Cristóbal', 'Monserrat', 'Flores', 'Parque Avellaneda', 'Constitución', 'Puerto Madero', 'Boedo', 'Villa Real', 'La Paternal', 'Villa Riachuelo'])
 ]
@@ -57,3 +56,21 @@ df[
     (df["rafaga_viento_max_direccion"].isin(['Noroeste', 'Oestenoroeste', 'Norte', 'Oeste', 'Nornoreste', 'Oestesuroeste' ])) &
     (df["barrio"].isin(['Parque Patricios', 'Villa Pueyrredón', 'Saavedra', 'Chacarita', 'Recoleta', 'Vélez Sársfield', 'Barracas', 'Villa del Parque', 'Villa Devoto', 'Caballito', 'Monte Castro', 'La Boca', 'Villa Soldati', 'San Cristóbal', 'Monserrat', 'Flores', 'Parque Avellaneda', 'Constitución', 'Puerto Madero', 'Boedo', 'Villa Real', 'La Paternal', 'Villa Riachuelo']))
 ]["llovieron_hamburguesas_al_dia_siguiente"].value_counts()
+
+
+# +
+def baseline( df ):
+    c = \
+        0.5 * (df["mm_lluvia_dia"].isna()) * 0.5 + \
+        0.5 * (df["llovieron_hamburguesas_hoy"]=="si") * 0.5 + \
+        0.3 * (df["horas_de_sol"]<7) * 0.3 + \
+        0.3 * (df["humedad_tarde"]>60) * 0.3 + \
+        0.3 *(df["nubosidad_tarde"]>7) * 0.3 + \
+        0.1*(df["rafaga_viento_max_direccion"].isin(['Noroeste', 'Oestenoroeste', 'Norte', 'Oeste', 'Nornoreste', 'Oestesuroeste' ])) + \
+        0.1*(df["barrio"].isin(['Parque Patricios', 'Villa Pueyrredón', 'Saavedra', 'Chacarita', 'Recoleta', 'Vélez Sársfield', 'Barracas', 'Villa del Parque', 'Villa Devoto', 'Caballito', 'Monte Castro', 'La Boca', 'Villa Soldati', 'San Cristóbal', 'Monserrat', 'Flores', 'Parque Avellaneda', 'Constitución', 'Puerto Madero', 'Boedo', 'Villa Real', 'La Paternal', 'Villa Riachuelo']))
+
+    return(c>1)
+_baseline = baseline(df)
+_target = df["llovieron_hamburguesas_al_dia_siguiente"]=="si"
+
+(_baseline == _target).mean()
