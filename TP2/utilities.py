6283a69b49cf9fa87246011ed1c056dbc2bbfbc3
nicolaszulaica <nzulaica@fi.uba.ar>
2021-12-04 23:31:58 -0300

    REFACTOR
    OPTIMIZE BY AUC-ROC
    
    - TREE
    - KNN
    - NB

--- TP2/utilities.py
+++ TP2/utilities.py
@@ -1,15 +1,60 @@
 # ### Imports
 
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, confusion_matrix
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+
+sns.set()
+
+# ### Code
+
+showPrints = False
 
 
 # ### Score
 
-def score(target, predictions):
-    print(f'ACCURACY: {accuracy_score(target, predictions)}')
+def fmtAvg( num, total ):
+    num = 100*float(num)/total
+    return f'{"%.2f" % num}%'
 
+
+def score(target, predictions=None, probas=None):
+    
+    if( type(probas) != type(None) ):
+        print(f'AUC-ROC: {roc_auc_score(target, predictions)}')
+
+    if( type(predictions) != type(None) ):
+        print(f'ACCURACY: {accuracy_score(target, predictions)}')
+        print(f'PRESICION: {precision_score(target, predictions)}')
+        print(f'RECALL: {recall_score(target, predictions)}')
+        print('CONFUSION MATRIX')
+        conf_matrix = confusion_matrix(target, predictions,labels=[1,0])
+        hmap=sns.heatmap(
+            conf_matrix,
+            cmap="YlOrBr",
+            annot=True,
+            yticklabels=['Real True','Real False'],
+            xticklabels=['Pred True','Pred False'],
+        )
+        tarSize = target.size
+        hmap.texts[0].set_text(fmtAvg(hmap.texts[0].get_text(),tarSize) + " TP")
+        hmap.texts[1].set_text(fmtAvg(hmap.texts[1].get_text(),tarSize) + " FN")
+        hmap.texts[2].set_text(fmtAvg(hmap.texts[2].get_text(),tarSize) + " FP")
+        hmap.texts[3].set_text(fmtAvg(hmap.texts[3].get_text(),tarSize) + " TN")
+
+
+true = pd.Series([True,True,True,False,False,False])
+pred = pd.Series([True,True,True,False,False,True])
+prob = np.array([[0.9,0.8,0.7,0.3,0.2,0.6]])
+
+if( showPrints ):
+    display(score(true,pred))
+
+if( showPrints ):
+    display(score(true,pred,prob))
 
 # ### Get Feat/ Targ
 
